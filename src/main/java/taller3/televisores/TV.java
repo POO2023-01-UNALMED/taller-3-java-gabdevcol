cc22c831d2e6f643f09f7e0c18d4991c19c63c29
gabdevcol <gabrielantonioserrano28@gmail.com>
2023-02-24 14:32:07 -0800

    intento de correcion del error de numtv

--- src/main/java/taller3/televisores/TV.java
+++ src/main/java/taller3/televisores/TV.java
@@ -8,7 +8,7 @@ public class TV {
 	private boolean estado;
 	private int volumen = 1;
 	private Control control;
-	private static int numTV = 0;
+	private static int numTV;
 	
 	public TV(Marca marca, boolean estado) {
 		this.marca = marca;
